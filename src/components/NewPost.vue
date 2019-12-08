06686f0f6ce753814630396924ef616402af9491
Kabrut <przemekturbak@gmail.com>
2019-12-08 02:20:37 +0100

    newpost

--- src/components/NewPost.vue
+++ src/components/NewPost.vue
@@ -1,20 +1,24 @@
 <template>
-    <div>
+    <div class='new'>
         <b-card>
             <b-card-header>
-                Nowy post
+                Nowy post 
             </b-card-header>
             <b-card-content>
                 <b-form-textarea/>
             </b-card-content>
-            <b-card-footer><b-button> Wyślij </b-button> %nbsp <b-button> Dodaj plik </b-button></b-card-footer>
+            <b-card-footer><b-button> Wyślij </b-button> &nbsp <b-button> Dodaj plik </b-button></b-card-footer>
         </b-card>
     </div>
 </template>
 <script>
 export default {
-    
 }
 </script>
 <style scoped>
+
+.new b-card{
+    background:cadetblue;
+}
+
 </style>
